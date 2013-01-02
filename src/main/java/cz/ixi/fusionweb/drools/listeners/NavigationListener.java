56b7b648ab7f380f4ef973af11caa592d3c0b387
Ixi <ixilecter@gmail.com>
2013-01-02 17:49:51 +0100

    Added customer menu and both menus css change.

--- src/main/java/cz/ixi/fusionweb/drools/listeners/NavigationListener.java
+++ src/main/java/cz/ixi/fusionweb/drools/listeners/NavigationListener.java
@@ -8,6 +8,7 @@ import cz.ixi.fusionweb.drools.model.ProductNavigationEvent;
 import cz.ixi.fusionweb.drools.rules.DroolsResourcesBean;
 import cz.ixi.fusionweb.entities.Product;
 import cz.ixi.fusionweb.entities.ProductCategory;
+import cz.ixi.fusionweb.entities.User;
 
 /**
  * Methods of this class are used for listening on components (links, buttons,
@@ -20,11 +21,15 @@ public class NavigationListener {
     @EJB
     DroolsResourcesBean drools;
 
+    public void product(Product product) {
+	drools.insertFact(new ProductNavigationEvent(product.getId()));
+    }
+
     public void category(ProductCategory category) {
 	drools.insertFact(new CategoryNavigationEvent(category.getId()));
     }
-    
-    public void product(Product product) {
-  	drools.insertFact(new ProductNavigationEvent(product.getId()));
-      }
+
+    public void customerMenuMyOrders(User user) {
+	System.out.println("User " + user.getUsername() + " clicked My Orders.");
+    }
 }
