415c059a2879527d5acf1bf598804186c7c8f688
Ixi <ixilecter@gmail.com>
2013-01-18 14:09:20 +0100

    Added possibility to log as default customer or administrator for easier development.

--- src/main/java/cz/ixi/fusionweb/web/UserController.java
+++ src/main/java/cz/ixi/fusionweb/web/UserController.java
@@ -1,4 +1,5 @@
 package cz.ixi.fusionweb.web;
+
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
@@ -13,131 +14,141 @@ import cz.ixi.fusionweb.ejb.UserBean;
 import cz.ixi.fusionweb.entities.User;
 import cz.ixi.fusionweb.web.util.JsfUtil;
 
-
 @SessionScoped
 @ManagedBean
 public class UserController implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String password;
-	private String username;
+    private String password;
+    private String username;
 
-	private User user;
+    private User user;
 
-	@Inject
-	private UserBean users;
+    @Inject
+    private UserBean users;
 
+    /**
+     * Constructor
+     */
+    public UserController() {
+    }
 
-	/**
-	 * Constructor
-	 */
-	public UserController() {
+    /**
+     * Performs login.
+     */
+    public String login() {
+
+	FacesContext context = FacesContext.getCurrentInstance();
+	HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+	try {
+	    if (request.getUserPrincipal() != null) {
+		request.logout();
+	    }
+	    request.login(this.username, this.password);
+	    System.out.println("Logged " + username);
+	    ;
+	    this.user = this.users.getUserByUsername(this.username);
+	    JsfUtil.addSuccessMessage("Login was successful.");
+	    return "/index";
+	} catch (ServletException ex) {
+
+	    JsfUtil.addErrorMessage("Login was unsuccessful.");
+	    System.out.println("Not logged, attempt was " + this.username + " " + this.password);
+	    ex.printStackTrace();
+	    return "/common/login";
 	}
+    }
 
-	/**
-	 * Performs login.
-	 */
-	public String login() {
+    /**
+     * Performs logout.
+     * 
+     * @return navigation page
+     */
+    public String logout() {
+	FacesContext context = FacesContext.getCurrentInstance();
+	HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
 
-		FacesContext context = FacesContext.getCurrentInstance();
-		HttpServletRequest request = (HttpServletRequest) context
-				.getExternalContext().getRequest();
-		try {
-			if (request.getUserPrincipal() != null) {
-				request.logout();
-			}
-			request.login(this.username, this.password);
-			System.out.println("Logged " + username);;
-			this.user = this.users.getUserByUsername(this.username);
-			JsfUtil.addSuccessMessage("Login was successful.");
-			return "/index";
-		} catch (ServletException ex) {
-		    
-			JsfUtil.addErrorMessage("Login was unsuccessful.");
-			System.out.println("Not logged, attempt was " + this.username + " "
-					+ this.password);
-			ex.printStackTrace();
-			return "/common/login";
-		}
+	try {
+	    this.user = null;
+	    request.logout();
+	    // clear the session
+	    ((HttpSession) context.getExternalContext().getSession(false)).invalidate();
+	    JsfUtil.addSuccessMessage("Logout was successful.");
+
+	} catch (ServletException ex) {
+	    JsfUtil.addErrorMessage("Logout was unsuccessful.");
 	}
+	return "/index";
 
-	/**
-	 * Performs logout.
-	 * 
-	 * @return navigation page
-	 */
-	public String logout() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		HttpServletRequest request = (HttpServletRequest) context
-				.getExternalContext().getRequest();
+    }
 
-		try {
-			this.user = null;
-			request.logout();
-			// clear the session
-			((HttpSession) context.getExternalContext().getSession(false))
-					.invalidate();
-			JsfUtil.addSuccessMessage("Logout was successful.");
+    /**
+     * Returns true if user is logged.
+     * 
+     * @return true if user is logged, false otherwise
+     */
+    public boolean isLogged() {
+	return (getUser() == null) ? false : true;
+    }
 
-		} catch (ServletException ex) {
-			JsfUtil.addErrorMessage("Logout was unsuccessful.");
-		}
-		return "/index";
+    /**
+     * Returns username.
+     * 
+     * @return username
+     */
+    public String getUsername() {
+	return this.username;
+    }
 
-	}
+    /**
+     * Sets username.
+     * 
+     * @param username
+     */
+    public void setUsername(String username) {
+	this.username = username;
+    }
 
-	/**
-	 * Returns true if user is logged.
-	 * 
-	 * @return true if user is logged, false otherwise
-	 */
-	public boolean isLogged() {
-		return (getUser() == null) ? false : true;
-	}
+    /**
+     * Returns password.
+     * 
+     * @return password
+     */
+    public String getPassword() {
+	return this.password;
+    }
 
-	/**
-	 * Returns username.
-	 * 
-	 * @return username
-	 */
-	public String getUsername() {
-		return this.username;
-	}
+    /**
+     * Sets password.
+     * 
+     * @param password
+     */
+    public void setPassword(String password) {
+	this.password = password;
+    }
 
-	/**
-	 * Sets username.
-	 * 
-	 * @param username
-	 */
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    /**
+     * Returns user.
+     * 
+     * @return user
+     */
+    public User getUser() {
+	return this.user;
+    }
 
-	/**
-	 * Returns password.
-	 * 
-	 * @return password
-	 */
-	public String getPassword() {
-		return this.password;
-	}
+    // HELPTING METHODS:
+    // TODO delete them at the end of the development
+    public String loginRick() {
+	this.username = "rick";
+	this.password = "1234";
+	return login();
+    }
+    
+    public String loginHugo() {
+   	this.username = "hugo";
+   	this.password = "1234";
+   	return login();
+       }
 
-	/**
-	 * Sets password.
-	 * 
-	 * @param password
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	/**
-	 * Returns user.
-	 * 
-	 * @return user
-	 */
-	public User getUser() {
-		return this.user;
-	}
 }
