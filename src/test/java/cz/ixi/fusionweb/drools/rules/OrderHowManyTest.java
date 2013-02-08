6cb3b412e1486b0347976311856d2956deafea3c
Ixi <ixilecter@gmail.com>
2013-02-08 15:14:39 +0100

    Added rule for daily statistics of orders and products.

--- src/test/java/cz/ixi/fusionweb/drools/rules/OrderHowManyTest.java
+++ src/test/java/cz/ixi/fusionweb/drools/rules/OrderHowManyTest.java
@@ -40,6 +40,7 @@ public class OrderHowManyTest {
     private KnowledgeBase kbase;
     private FiredRulesListener firedRules;
     private StatisticsRecordHourlyChannelMock statisticsHourly;
+    private StatisticsRecordDailyChannelMock statisticsDaily;
 
     @Before
     public void setUp() {
@@ -66,6 +67,9 @@ public class OrderHowManyTest {
 
 	statisticsHourly = new StatisticsRecordHourlyChannelMock();
 	ksession.registerChannel("statisticsHourly", statisticsHourly);
+
+	statisticsDaily = new StatisticsRecordDailyChannelMock();
+	ksession.registerChannel("statisticsDaily", statisticsDaily);
     }
 
     @After
@@ -78,7 +82,7 @@ public class OrderHowManyTest {
     }
 
     @Test
-    public void reportRightNumberOfBoughtProductsAndCreatedOrders() {
+    public void reportRightNumberOfBoughtProductsAndCreatedOrdersInLastHour() {
 	String rule = "Report how many orders were created and items bought in in the last hour";
 	SessionPseudoClock clock = ksession.getSessionClock();
 	Calendar cal = new GregorianCalendar(2013, 01, 01, 14, 0, 0);
@@ -88,10 +92,10 @@ public class OrderHowManyTest {
 	clock.advanceTime(5, TimeUnit.MINUTES);
 	ksession.fireAllRules();
 	assertEquals(1, firedRules.howManyTimesIsRuleFired(rule));
-	assertEquals(1, statisticsHourly.getCreatedNotifications());
+	assertEquals(1, statisticsHourly.getCreatedStatistics());
 	assertTrue(statisticsHourly.getDescription().contains("0 order(s)"));
 	assertTrue(statisticsHourly.getDescription().contains("0 product(s)"));
-	
+
 	OrderCreatedEvent oce = new OrderCreatedEvent(1, "rick", 2, 60.0);
 	oce.setTime(new Date(clock.getCurrentTime()));
 	ksession.insert(oce);
@@ -105,27 +109,25 @@ public class OrderHowManyTest {
 	oce = new OrderCreatedEvent(2, "rick2", 1, 50.0);
 	oce.setTime(new Date(clock.getCurrentTime()));
 	ksession.insert(oce);
-	pbe = new  ProductBoughtEvent(2, 3, "rick2", "dvd");
+	pbe = new ProductBoughtEvent(2, 3, "rick2", "dvd");
 	pbe.setTime(new Date(clock.getCurrentTime()));
 	ksession.insert(pbe);
-
 
 	ksession.fireAllRules();
 
 	clock.advanceTime(55, TimeUnit.MINUTES);
 
 	assertEquals(2, firedRules.howManyTimesIsRuleFired(rule));
-	assertEquals(2, statisticsHourly.getCreatedNotifications());
+	assertEquals(2, statisticsHourly.getCreatedStatistics());
 	assertTrue(statisticsHourly.getDescription().contains("2 order(s)"));
 	assertTrue(statisticsHourly.getDescription().contains("3 product(s)"));
 
 	clock.advanceTime(5, TimeUnit.MINUTES);
 
-	
 	oce = new OrderCreatedEvent(3, "rick2", 4, 50.0);
 	oce.setTime(new Date(clock.getCurrentTime()));
 	ksession.insert(oce);
-	
+
 	for (int i = 1; i <= 4; i++) {
 	    ProductBoughtEvent pbe2 = new ProductBoughtEvent(3, 4, "rick3", "dvd");
 	    pbe2.setTime(new Date(clock.getCurrentTime()));
@@ -135,32 +137,114 @@ public class OrderHowManyTest {
 
 	clock.advanceTime(55, TimeUnit.MINUTES);
 	assertEquals(3, firedRules.howManyTimesIsRuleFired(rule));
-	assertEquals(3, statisticsHourly.getCreatedNotifications());
+	assertEquals(3, statisticsHourly.getCreatedStatistics());
 	assertTrue(statisticsHourly.getDescription().contains("1 order(s)"));
 	assertTrue(statisticsHourly.getDescription().contains("4 product(s)"));
     }
+
+    @Test
+    public void reportRightNumberOfBoughtProductsAndCreatedOrdersInLastDay() {
+	String rule = "Report how many orders were created and items bought in in the last day";
+	SessionPseudoClock clock = ksession.getSessionClock();
+	Calendar cal = new GregorianCalendar(2013, 01, 01, 14, 0, 0);
+	clock.advanceTime(cal.getTimeInMillis(), TimeUnit.MILLISECONDS);
+	ksession.fireAllRules();
+
+	clock.advanceTime(5, TimeUnit.MINUTES);
+
+	OrderCreatedEvent oce = new OrderCreatedEvent(1, "rick", 2, 60.0);
+	oce.setTime(new Date(clock.getCurrentTime()));
+	ksession.insert(oce);
+	ProductBoughtEvent pbe = new ProductBoughtEvent(1, 1, "rick", "dvd");
+	pbe.setTime(new Date(clock.getCurrentTime()));
+	ksession.insert(pbe);
+	pbe = new ProductBoughtEvent(1, 2, "rick", "cd");
+	pbe.setTime(new Date(clock.getCurrentTime()));
+	ksession.insert(pbe);
+
+	oce = new OrderCreatedEvent(2, "rick2", 1, 50.0);
+	oce.setTime(new Date(clock.getCurrentTime()));
+	ksession.insert(oce);
+	pbe = new ProductBoughtEvent(2, 3, "rick2", "dvd");
+	pbe.setTime(new Date(clock.getCurrentTime()));
+	ksession.insert(pbe);
+
+	ksession.fireAllRules();
+
+	clock.advanceTime(10, TimeUnit.HOURS);
+	
+	ksession.fireAllRules();
+
+	assertEquals(1, firedRules.howManyTimesIsRuleFired(rule));
+	assertEquals(1, statisticsDaily.getCreatedStatistics());
+	assertTrue(statisticsDaily.getDescription().contains("2 order(s)"));
+	assertTrue(statisticsDaily.getDescription().contains("3 product(s)"));
+
+	clock.advanceTime(5, TimeUnit.MINUTES);
+
+	oce = new OrderCreatedEvent(3, "rick2", 4, 50.0);
+	oce.setTime(new Date(clock.getCurrentTime()));
+	ksession.insert(oce);
+
+	for (int i = 1; i <= 4; i++) {
+	    ProductBoughtEvent pbe2 = new ProductBoughtEvent(3, 4, "rick3", "dvd");
+	    pbe2.setTime(new Date(clock.getCurrentTime()));
+	    ksession.insert(pbe2);
+	}
+	ksession.fireAllRules();
+
+	clock.advanceTime(1, TimeUnit.DAYS);
+	assertEquals(2, firedRules.howManyTimesIsRuleFired(rule));
+	assertEquals(2, statisticsDaily.getCreatedStatistics());
+	assertTrue(statisticsDaily.getDescription().contains("1 order(s)"));
+	assertTrue(statisticsDaily.getDescription().contains("4 product(s)"));
+    }
+
     private class StatisticsRecordHourlyChannelMock implements Channel {
 
-  	private int createdStatistics;
-  	private String description;
+	private int createdStatistics;
+	private String description;
 
-  	public int getCreatedNotifications() {
-  	    return createdStatistics;
-  	}
+	public int getCreatedStatistics() {
+	    return createdStatistics;
+	}
 
-  	public String getDescription() {
-  	    return description;
-  	}
+	public String getDescription() {
+	    return description;
+	}
 
-  	public void setDescription(String description) {
-  	    this.description = description;
-  	}
+	public void setDescription(String description) {
+	    this.description = description;
+	}
 
-  	@Override
-  	public void send(Object object) {
-  	    setDescription(object.toString());
-  	    createdStatistics++;
-  	}
-      }
+	@Override
+	public void send(Object object) {
+	    setDescription(object.toString());
+	    createdStatistics++;
+	}
+    }
 
+    private class StatisticsRecordDailyChannelMock implements Channel {
+
+	private int createdStatistics;
+	private String description;
+
+	public int getCreatedStatistics() {
+	    return createdStatistics;
+	}
+
+	public String getDescription() {
+	    return description;
+	}
+
+	public void setDescription(String description) {
+	    this.description = description;
+	}
+
+	@Override
+	public void send(Object object) {
+	    setDescription(object.toString());
+	    createdStatistics++;
+	}
+    }
 }
